95f97c706d5437ecb0838a7e9c9833149de17df4
kaushalya <s16330@sci.pdn.ac.lk>
2020-10-03 21:18:11 +0530

    created signup screen

--- screens/auth/SignUpScreen.js
+++ screens/auth/SignUpScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as React from "react";
 import { Text, View, TextInput, Button, StyleSheet, TouchableOpacity, Platform, Alert } from "react-native";
 import { FirebaseRecaptchaVerifierModal } from "expo-firebase-recaptcha";
@@ -88,4 +89,81 @@ export default function SignUpScreen({firebaseConfig}) {
       ) : undefined}
     </View>
     )
+=======
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    TextInput,
+    KeyboardAvoidingView,
+    Keyboard,
+    TouchableWithoutFeedback, Image,
+    Alert
+
+} from 'react-native'
+
+const SignUpScreen=()=>{
+    const [mobileNumber, setMobileNumber] = React.useState('');
+
+    const [value, onChangeText] = React.useState('Enter Your Email or Phone No');
+    return (
+        <View style={styles.container}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <KeyboardAvoidingView behavior="position" style={styles.loginContainer}>
+
+
+            <Image
+                style={{width:200,marginHorizontal:70,marginVertical:20,}}
+                resizeMode="contain"
+                source={require('../../assets/images/logo.png')}
+            />
+
+            <Text style={styles.regTitleTextStyle}>Register New User</Text>
+            <TextInput
+                placeholder="Enter Your Email or Phone Number"
+                style={styles.textInput}
+                value={mobileNumber}
+                onChangeText={setMobileNumber}
+            />
+
+            <Button title='Register' onPress={()=>{
+                Alert.alert('Register Success!','Congratulations!');
+            }}/>
+                </KeyboardAvoidingView>
+            </TouchableWithoutFeedback>
+        </View>
+    );
+>>>>>>> signupScreen
 }
+
+
+const styles=StyleSheet.create({
+    regTitleTextStyle:{
+        fontSize:20,
+        color: 'white',
+    },
+    container:{
+        flex:1,
+        backgroundColor:'#0071bd',
+    },
+    textInput:{
+        marginVertical:10,
+        padding:10,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        backgroundColor:'#fff',
+        color:'#000',
+        borderRadius:4,
+    },
+
+    loginContainer:{
+        marginTop:200,
+        marginHorizontal:40,
+    },
+});
+
+export default SignUpScreen;
+
