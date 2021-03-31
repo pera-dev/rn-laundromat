b4ee8e2fe0c2a706cc30b140f84d80ca82ea456b
kalpana RI <s16368@sci.pdn.ac.lk>
2021-03-31 14:00:42 +0530

    Update OrderStatus

--- screens/OrderStatusScreen.js
+++ screens/OrderStatusScreen.js
@@ -1,16 +1,46 @@
-import * as React from 'react'
-import { ScrollView } from 'react-native-gesture-handler'
-import { Text ,View} from 'react-native'
-import ButtonLogin from '../components/ButtonLogin'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-
-export default function OrderStatusScreen(navigation) {
-    return (
-        <View>
-            <ScrollView>
-                <Text>Order Status</Text>
-                <ButtonLogin/>
-            </ScrollView>
-        </View>
-    )
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>My Orders</Text>
+      <View style={styles.order1}>
+        <Text>Order1 </Text>
+      </View>
+      
+      
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 50,
+    flex: 1,
+    backgroundColor: "#fff259",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  order1: {
+    borderWidth: 1,
+    padding:50,
+    borderColor:"#000",
+    borderRadius:20,
+    alignItems: "center",
+    marginTop: 150,
+    marginBottom: 15,
+  },
+  order: {
+    borderWidth: 1,
+    padding:50,
+    borderColor:"#000",
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 15,
+    borderRadius:20,
+
+  },
+});
