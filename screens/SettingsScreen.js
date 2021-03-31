a5803f90bf53e280b505a29082740822e4a8654b
CWxMaxX <s16538@sci.pdn.ac.lk>
2021-03-31 13:23:19 +0530

    Modify Settings Page

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,23 +1,54 @@
-import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Avatar } from 'react-native-elements'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function SettingsScreen() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.contentContainer}>
-            
-            </View>
-        </View>
-    )
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Settings</Text>
+      <View style={styles.option1}>
+        <Text>Account </Text>
+      </View>
+      <View style={styles.option}>
+        <Text>Notifications </Text>
+      </View>
+      <View style={styles.option}>
+        <Text>Apperance </Text>
+      </View>
+      <View style={styles.option}>
+        <Text>Privacy and Securty </Text>
+      </View>
+      <View style={styles.option}>
+        <Text>Help and Support</Text>
+      </View>
+      <View style={styles.option}>
+        <Text>About</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1
-    },
-    contentContainer:{
-        paddingTop:30 ,
-        marginTop:50,
-    }
-})
+  container: {
+    padding: 50,
+    flex: 1,
+    backgroundColor: "#fff259",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  option1: {
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
+    alignItems: "center",
+    marginTop: 150,
+    marginBottom: 15,
+  },
+  option: {
+    borderBottomColor: "#000",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    marginTop: 10,
+    marginBottom: 15,
+  },
+});
